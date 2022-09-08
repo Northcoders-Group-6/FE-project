0dce0d290d15bd9a05013905cb8d2c239dae7384
Jodiech <jodiecheung1@gmail.com>
2022-09-08 10:13:18 +0100

    which user navigate

---
+++ screens/StudentReg.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const StudentReg = () => {
+  return (
+    <View>
+      <Text>StudentReg</Text>
+    </View>
+  );
+};
+
+export default StudentReg;
+
+const styles = StyleSheet.create({});
