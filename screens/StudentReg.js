157085bb8f5af9dbf65a8290289ecc218e98c994
Ha-Mundo <trauzzu@gmail.com>
2022-09-09 20:03:31 +0100

    Formik elements   + onSubmit to db func added

--- screens/StudentReg.js
+++ screens/StudentReg.js
@@ -1,74 +1,118 @@
+import { db } from "../firebase";
+
+import { Formik } from "formik";
 import {
+  Button,
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
   KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
-import React from "react";
-import { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
 
 const StudentReg = () => {
-  const [firstName, setFirstName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [studentId, setStudentId] = useState("");
-  const [location, setLocation] = useState("");
-  const [email, setEmail] = useState("");
-  const [telephone, setTelephone] = useState("");
-
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior={Platform.select({ android: undefined, ios: "padding" })}
-    >
-      <View style={styles.buttonContainer}>
-        <TextInput
-          placeholder="First Name"
-          value={firstName}
-          onChangeText={(text) => setFirstName(text)}
-        ></TextInput>
-        <TextInput
-          placeholder="Surname"
-          value={surname}
-          onChangeText={(text) => setSurname(text)}
-        ></TextInput>
-        <TextInput
-          placeholder="Student ID number"
-          value={studentId}
-          onChangeText={(text) => setStudentId(text)}
-        ></TextInput>
-        <TextInput
-          placeholder="Location"
-          value={location}
-          onChangeText={(text) => setLocation(text)}
-        ></TextInput>
-        <TextInput
-          placeholder="Email Address"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        ></TextInput>
-        <TextInput
-          placeholder="Telephone"
-          value={telephone}
-          onChangeText={(text) => setTelephone(text)}
-        ></TextInput>
-        <TouchableOpacity>
-          <Text style={[styles.buttonOutlineText, styles.button]}>Submit</Text>
-        </TouchableOpacity>
-        <Text>
-          By signing up I agreee to Voluntreat's terms of service and privacy
-          policy
-        </Text>
-      </View>
-    </KeyboardAvoidingView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.select({ android: undefined, ios: "padding" })}
+      >
+        <View style={styles.buttonContainer}>
+          <Formik
+            initialValues={{
+              firstName: "",
+              surname: "",
+              studentId: "",
+              location: "",
+              email: "",
+              phone: "",
+              password: "",
+              userType: "vol",
+            }}
+            onSubmit={(values, actions) => {
+              console.log(values);
+              db.collection("Volunteers").add(values);
+              actions.resetForm();
+            }}
+          >
+            {props => (
+              <View>
+                <TextInput
+                  style={styles.input}
+                  placeholder="First Name"
+                  onChangeText={props.handleChange("firstName")}
+                  value={props.values.firstName}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Surname"
+                  onChangeText={props.handleChange("surname")}
+                  value={props.values.surname}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Student ID number"
+                  onChangeText={props.handleChange("studentId")}
+                  value={props.values.studentId}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Location"
+                  onChangeText={props.handleChange("location")}
+                  value={props.values.location}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Email"
+                  onChangeText={props.handleChange("email")}
+                  value={props.values.email}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Phone"
+                  onChangeText={props.handleChange("phone")}
+                  value={props.values.phone}
+                  keyboardType="numeric"
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Password"
+                  onChangeText={props.handleChange("password")}
+                  value={props.values.password}
+                />
+                <TouchableOpacity>
+                  <Button
+                    style={styles.button}
+                    title="Submit"
+                    onPress={props.handleSubmit}
+                  />
+                </TouchableOpacity>
+                <Text>
+                  By signing up I agreee to Voluntreat's terms of service and
+                  privacy policy
+                </Text>
+              </View>
+            )}
+          </Formik>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
 
 export default StudentReg;
 
 const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    padding: 10,
+    fontSize: 18,
+    borderRadius: 6,
+  },
   buttonContainer: {
     width: "60%",
     justifyContent: "center",
