14bf7ed385b6d8fb810e9e8c33bbb8826863e9a2
Jodiech <jodiecheung1@gmail.com>
2022-09-12 23:32:38 +0100

    changed name unicorn

--- screens/OrgYourEvents.js
+++ screens/OrgYourEvents.js
@@ -8,7 +8,7 @@ const OrgYourEvents = () => {
     {
       img: "https://images.unsplash.com/photo-1628717341663-0007b0ee2597?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80.jpeg",
       opp: "Food Bank Donation",
-      company: "Green Veg Grocers",
+      company: "Unicorn",
       location: "Oxford Road, Manchester",
     },
     {
